8b0b6849793f675921de8ec0d14660fef66c62e1
Joe Fifer <jfifer@gmail.com>
2018-09-15 14:08:46 -0400

    Nav styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,16 +16,27 @@ class Header extends Component {
   render() {
     return (
       <div className="Nav">
-        <Navbar>
+        <Navbar className='topbar'>
           <Navbar.Header>
             <Navbar.Brand>
               <Image height='30' src={ logo } />
             </Navbar.Brand>
           </Navbar.Header>
           <Nav pullRight>
-            <FormGroup>
-              <FormControl id='search' name='search' />
+            <FormGroup className='has-feedback'>
+              <FormControl id='search' name='search' placeholder='Search Partners' />
+              <span className='pull-left glyphicon glyphicon-search form-control-feedback'></span>
             </FormGroup>
+          </Nav>
+        </Navbar>
+        <Navbar className='router'>
+          <Nav>
+            <NavItem eventKey={1} href='/vendors'>Vendors</NavItem>
+            <NavItem eventKey={2} href='/talent'>Talent</NavItem>
+            <NavItem eventKey={3} href='/agent'>Agent</NavItem>
+            <NavItem eventKey={4} href='/resources'>Resources</NavItem>
+            <NavItem eventKey={5} href='/faq'>FAQ</NavItem>
+            <NavItem eventKey={6} href='/guide'>Best Practices/Guidebook</NavItem>
           </Nav>
         </Navbar>
       </div>
