5ac805730b489f6d64a80169aca2ce8225d83f0b
Joe Fifer <jfifer@gmail.com>
2018-09-17 11:55:08 -0400

    vendors styling

--- src/components/Vendors.js
+++ src/components/Vendors.js
@@ -1,9 +1,154 @@
 import React, { Component } from 'react';
+import './assets/style/vendors.css';
   
 class Vendors extends Component {
   render() {
     return (
-      <h3>Vendors</h3>
+      <div>
+        <h3>Vendors</h3>
+        <div className="vendors">
+          <div className="row">
+            <div class="col-lg-3 col-md-2 col-sm-4 col-xs-8">
+              <div className="panel panel-default">
+                <div className="panel-heading">
+                  <h3>Photo</h3>
+                </div>
+                <div className="panel-content">
+                  <h4>Vendor Name</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
+                  <ul>
+                    <li>Meta: <span>Data</span></li>
+                    <li>Meta: <span>Data</span></li>
+                    <li>Meta: <span>Data</span></li>
+                    <li>Meta: <span>Data</span></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-2 col-sm-4 col-xs-8">
+              <div className="panel panel-default">
+                <div className="panel-heading">
+                  <h3>Photo</h3>
+                </div>
+                <div className="panel-content">
+                  <h4>Vendor Name</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
+                  <ul>
+                    <li>Meta: <span>Data</span></li>
+                    <li>Meta: <span>Data</span></li>
+                    <li>Meta: <span>Data</span></li>
+                    <li>Meta: <span>Data</span></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-2 col-sm-4 col-xs-8">
+              <div className="panel panel-default">
+                <div className="panel-heading">
+                  <h3>Photo</h3>
+                </div>
+                <div className="panel-content">
+                  <h4>Vendor Name</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
+                  <ul>
+                    <li>Meta: <span>Data</span></li>
+                    <li>Meta: <span>Data</span></li>
+                    <li>Meta: <span>Data</span></li>
+                    <li>Meta: <span>Data</span></li>
+                  </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-2 col-sm-4 col-xs-8">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h3>Photo</h3>
+              </div>
+              <div className="panel-content">
+                <h4>Vendor Name</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
+                <ul>
+                  <li>Meta: <span>Data</span></li>
+                  <li>Meta: <span>Data</span></li>
+                  <li>Meta: <span>Data</span></li>
+                  <li>Meta: <span>Data</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div class="col-lg-3 col-md-2 col-sm-4 col-xs-8">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h3>Photo</h3>
+              </div>
+              <div className="panel-content">
+                <h4>Vendor Name</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
+                <ul>
+                  <li>Meta: <span>Data</span></li>
+                  <li>Meta: <span>Data</span></li>
+                  <li>Meta: <span>Data</span></li>
+                  <li>Meta: <span>Data</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-2 col-sm-4 col-xs-8">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h3>Photo</h3>
+              </div>
+              <div className="panel-content">
+                <h4>Vendor Name</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
+                <ul>
+                  <li>Meta: <span>Data</span></li>
+                  <li>Meta: <span>Data</span></li>
+                  <li>Meta: <span>Data</span></li>
+                  <li>Meta: <span>Data</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-2 col-sm-4 col-xs-8">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h3>Photo</h3>
+              </div>
+              <div className="panel-content">
+                <h4>Vendor Name</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
+                <ul>
+                  <li>Meta: <span>Data</span></li>
+                  <li>Meta: <span>Data</span></li>
+                  <li>Meta: <span>Data</span></li>
+                  <li>Meta: <span>Data</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-2 col-sm-4 col-xs-8">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h3>Photo</h3>
+              </div>
+              <div className="panel-content">
+                <h4>Vendor Name</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
+                <ul>
+                  <li>Meta: <span>Data</span></li>
+                  <li>Meta: <span>Data</span></li>
+                  <li>Meta: <span>Data</span></li>
+                  <li>Meta: <span>Data</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
